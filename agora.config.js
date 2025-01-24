9e39028e4c10774691d9d7f033d5b25d22cc7e9d
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-01-24 10:12:22 +0100

    feat: add env config

---
+++ agora.config.js
@@ -0,0 +1,16 @@
+export const ENV = {
+    LOCAL: {
+        "name": "Local",
+        "host": "http://localhost:8000",
+        "company_id": 1,
+        "api_key": "627a78c4ff6c774b1f50079c4302dccacd716b09",
+    },
+
+    UNITE: {
+        "name": "Unite!",
+        "host": "https://agora.unite-university.eu",
+        "company_id": 1,
+    }
+}
+
+export const CONFIG = ENV.LOCAL
