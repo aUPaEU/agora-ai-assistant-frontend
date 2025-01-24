2442316179c1e847922cd5e77918df0621c00b4c
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-01-24 10:08:01 +0100

    feat: add api functions to fetch acceleration services and elements

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,11 +1,74 @@
-import { LIST_ACCELERATION_SERVICE, ACCELERATION_SERVICES } from "../data/mock.data"
+import { ACCELERATION_SERVICES, STRUCTURED_DATA } from "../data/mock.data"
+import { CONFIG, CURRENT_CONFIG } from "../../agora.config"
 
 export const fetchAccelerationServices = async () => {
+    try {
+        //const response = ACCELERATION_SERVICES
+        const response = STRUCTURED_DATA
+        /* const data = await response.json()
+        return data */
+        return response
+    }
+
+    catch (error) {
+        throw error
+    }
+}
+
+export const fetchAccelerationServicesV1 = async () => {
     try {
         const response = ACCELERATION_SERVICES
         /* const data = await response.json()
         return data */
         return response
+    }
+
+    catch (error) {
+        throw error
+    }
+}
+
+export const fetchAgoraServices = async () => {
+    try {
+        const url = `${CONFIG.host}/catalogue-api/v1/acceleration-services/${CONFIG.company_id}`
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('Something went wrong while fetching Agora services')
+        }
+
+        const data = await response.json()
+        return data
+    }
+
+    catch (error) {
+        throw error
+    }
+}
+
+export const fetchElement = async (model, id, fields=null) => {
+    const COMMON_FIELDS = [
+        'id',
+        'name',
+        'description',
+        'summary',
+        'university_origin'
+    ]
+
+    try {
+        const url = `${CONFIG.host}/catalogue-api/v1/ai/${model}/${id}?fields=${fields 
+            ? `${COMMON_FIELDS.concat(fields)}` 
+            : `${COMMON_FIELDS}`
+        }`
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('Something went wrong while fetching element')
+        }
+
+        const data = await response.json()
+        console.log("DATATATATA: ", data)
+        return data
     }
 
     catch (error) {
