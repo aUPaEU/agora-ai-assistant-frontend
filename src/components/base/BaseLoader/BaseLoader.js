ab503ae12fb0b011b7cc4be10b4ad854374cc923
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-01-24 10:12:36 +0100

    feat: add a loader component

---
+++ src/components/base/BaseLoader/BaseLoader.js
@@ -0,0 +1,22 @@
+import { PlainComponent } from "plain-reactive"
+import { PATHS } from "../../../constants/paths.const"
+import { html } from "../../../utils/templateTags.util"
+
+class BaseLoader extends PlainComponent {
+    constructor() {
+        super('agora-base-loader', `${PATHS.BASE_COMPONENTS}/BaseLoader/BaseLoader.css`)
+    }
+
+    template() {
+        return html`
+            <img 
+                width=${this.getAttribute('width')} 
+                height=${this.getAttribute('height')} 
+                src="public/assets/images/aupaeu-logo.png" 
+                alt="Agora"
+            >
+        `
+    }
+}
+
+export default window.customElements.define('agora-base-loader', BaseLoader)
