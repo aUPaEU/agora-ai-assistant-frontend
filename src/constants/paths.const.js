8e0d35acecae2faf54e72ec95686265cd903fd48
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-01-30 15:00:18 +0100

    fix: update bundle with new absolute paths to the cdn

--- src/constants/paths.const.js
+++ src/constants/paths.const.js
@@ -1,4 +1,6 @@
-export const PATHS = {
+import { CONFIG } from "../../agora.config"
+
+const LOCAL_PATHS = {
     SRC : 'src',
     DIST: 'dist',
     PUBLIC: 'public',
@@ -8,3 +10,18 @@ export const PATHS = {
     LAYOUT_COMPONENTS: 'src/components/layout',
     PAGE_COMPONENTS: 'src/components/page',
 }
+
+const CDN_URL = 'https://cdn.jsdelivr.net/gh/aUPaEU/agora-ai-assistant-frontend@main'
+
+const CDN_PATHS = {
+    SRC : `${CDN_URL}/src`,
+    DIST: `${CDN_URL}/dist`,
+    PUBLIC: `${CDN_URL}/public`,
+    BASE_COMPONENTS: `${CDN_URL}/src/components/base`,
+    MID_COMPONENTS: `${CDN_URL}/src/components/mid`,
+    COMPLEX_COMPONENTS: `${CDN_URL}/src/components/complex`,
+    LAYOUT_COMPONENTS: `${CDN_URL}/src/components/layout`,
+    PAGE_COMPONENTS: `${CDN_URL}/src/components/page`,
+}
+
+export const PATHS = CDN_PATHS
