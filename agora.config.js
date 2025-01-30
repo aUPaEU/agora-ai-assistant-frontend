50d65354d14e5a2c883f3d52be7a95c004c8ef7b
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-01-30 10:01:32 +0100

    feat: add configuration for DEV environment

--- agora.config.js
+++ agora.config.js
@@ -1,16 +1,24 @@
+// This could be fetched automatically from the server depending on an environment variable,
+// a cookie or the domain name.
+
 export const ENV = {
     LOCAL: {
-        "name": "Local",
+        "name": "Unite!",
         "host": "http://localhost:8000",
         "company_id": 1,
-        "api_key": "627a78c4ff6c774b1f50079c4302dccacd716b09",
     },
 
     UNITE: {
         "name": "Unite!",
         "host": "https://agora.unite-university.eu",
         "company_id": 1,
+    },
+
+    CIVIS_DEV: {
+        "name": "CIVIS",
+        "host": "https://aupaeu-dev.widening.eu",
+        "company_id": 1,
     }
 }
 
-export const CONFIG = ENV.LOCAL
+export const CONFIG = ENV.CIVIS_DEV
