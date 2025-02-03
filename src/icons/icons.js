a6b1de6f2ad5ea2b274cc22c03e180035038e6d9
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-02-03 17:13:48 +0100

    style: add arrow icons

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -143,3 +143,15 @@ export const INFO = html`
     <circle cx="12" cy="8" r="1" fill="currentColor"/>
     </svg>
 `
+
+export const ARROW_LEFT = html`
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15 6L9 12L15 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+`
+
+export const ARROW_RIGHT = html`
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9 6L15 12L9 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+`
