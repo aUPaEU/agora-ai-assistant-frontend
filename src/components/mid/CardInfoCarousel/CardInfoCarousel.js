063045b96671824e3d5dd5f5694178af7de27636
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-02-17 18:11:26 +0100

    feat: implement navigation controls

--- src/components/mid/CardInfoCarousel/CardInfoCarousel.js
+++ src/components/mid/CardInfoCarousel/CardInfoCarousel.js
@@ -16,7 +16,7 @@ class CardInfoCarousel extends PlainComponent {
 
         this.resultContext = new PlainContext('result', this, true)
 
-        this.groupData = new PlainState(null, this) // We'll use this to store all the cards in a group so we can display them in the carousel and be able to scroll through them
+        this.groupedData = new PlainState(null, this) // We'll use this to store all the cards in a group so we can display them in the carousel and be able to scroll through them
         this.displayedCardId = new PlainState(null, this)
 
         this.signals = new PlainSignal(this)
@@ -56,31 +56,28 @@ class CardInfoCarousel extends PlainComponent {
     listeners() {
         this.wrapper.onclick = (e) => this.close(e)
 
+        this.$('.next-control').onclick = (e) => this.showNext(e)
+        this.$('.prev-control').onclick = (e) => this.showPrevious(e)
+
         this.$('.card-info-explore-button').onclick = () => window.open(this.$('.card-info-explore-button').dataset.url, '_blank')
     }
 
     show(id, detailUrl, hasImage) {
-        const services = this.resultContext.getData('data') 
-            ? [...new Set(this.resultContext.getData('data').map(result => result.service))].sort()
-            : []
-
-        const data = (() => {
-            return services.map(service => {
-                const items = this.resultContext.getData('data').filter(result => result.service === service)
-                return {
-                    service: service,
-                    items: items
-                }
-            })
-        })()
-
-        // We make the dialog visible
-        this.style.display = 'block'
+        this.displayedCardId.setState(id, false)
 
         const cardData = this.resultContext.getData('data').find(card => Number(card.data.id) === Number(id))
-        console.log("CARD DATA:", cardData)
         
+        this.updateVisibility(hasImage)
+        this.updateCardConent(cardData, detailUrl)
+        this.displayAdditionalFields(cardData.data, cardData.featured_fields)
+        this.updateNavigationControls()
+    }
+
+    updateVisibility(hasImage) {
+        // We make the dialog visible
+        this.style.display = 'block'
         this.$('.card-info-wrapper').classList.add('fade-in')
+
         this.wrapper.classList.remove('no-image')
 
         // Check if the card has an image
@@ -88,7 +85,10 @@ class CardInfoCarousel extends PlainComponent {
             this.wrapper.classList.add('no-image')
         }
 
-        // Fill the card info
+        this.wrapper.style.display = 'flex'
+    }
+
+    updateCardConent(cardData, detailUrl) {
         this.$('.card-info-image').src = `${CONFIG.host}/web/image?model=${cardData.model}&id=${cardData.data.id}&field=image` ?? ''
         this.$('.card-info-name').textContent = cardData.data.name ?? ''
         this.$('.card-info-lastname').innerHTML = cardData.data.lastname ?? ''
@@ -96,18 +96,53 @@ class CardInfoCarousel extends PlainComponent {
         this.$('.card-info-origin').textContent = cardData.data.origin ?? ''
         this.$('.card-info-explore-button').dataset['url'] = detailUrl ?? ''
 
-        //this.displayAdditionalFields(cardData.data, cardData.featured_fields)
-
         this.$('.card-info-content').scrollTo(0, 0)
-        this.wrapper.style.display = 'flex'
     }
 
-    showNext() {
-
+    updateNavigationControls() {
+        this.toogleNextControl()
+        this.tooglePreviousControl()
     }
 
-    showPrevious() {
+    showNext(e) {
+        const cardData = this.resultContext.getData('data').find(card => Number(card.data.id) === Number(this.displayedCardId.getState()))
+        const currentService = this.groupedData.getState().find(group => group.service === cardData.service)
+        const cardIndex = currentService.items.findIndex(item => Number(item.data.id) === Number(cardData.data.id))
 
+        if (cardIndex === currentService.items.length - 1) return
+
+        this.displayedCardId.setState(currentService.items[cardIndex + 1].data.id, false)
+        this.show(this.displayedCardId.getState(), cardData.data.detail_url, cardData.data.image_url)
+        // this.handleAnimation(e, 'next')
+    }
+
+    showPrevious(e) {
+        const cardData = this.resultContext.getData('data').find(card => Number(card.data.id) === Number(this.displayedCardId.getState()))
+        const currentService = this.groupedData.getState().find(group => group.service === cardData.service)
+        const cardIndex = currentService.items.findIndex(item => Number(item.data.id) === Number(cardData.data.id))
+
+        if (cardIndex === 0) return
+
+        this.displayedCardId.setState(currentService.items[cardIndex - 1].data.id, false)
+        this.show(this.displayedCardId.getState(), cardData.data.detail_url, cardData.data.image_url)
+        // this.handleAnimation(e, 'prev')
+    }
+
+    toogleNextControl() {
+        const cardData = this.resultContext.getData('data').find(card => Number(card.data.id) === Number(this.displayedCardId.getState()))
+        const currentService = this.groupedData.getState().find(group => group.service === cardData.service)
+        const cardIndex = currentService.items.findIndex(item => Number(item.data.id) === Number(cardData.data.id))
+
+        this.$('.next-control').classList.toggle('hidden', cardIndex === currentService.items.length - 1)
+    }
+
+    tooglePreviousControl() {
+        const cardData = this.resultContext.getData('data').find(card => Number(card.data.id) === Number(this.displayedCardId.getState()))
+        const currentService = this.groupedData.getState().find(group => group.service === cardData.service)
+        const cardIndex = currentService.items.findIndex(item => Number(item.data.id) === Number(cardData.data.id))
+
+        // We hide the previous button if we're at the first card
+        this.$('.prev-control').classList.toggle('hidden', cardIndex === 0)
     }
 
     close(e) {
@@ -118,8 +153,7 @@ class CardInfoCarousel extends PlainComponent {
     }
 
     setData(data) {
-        this.data.setState(data, false)
-        console.log("Data fetched by the result window into the card info carousel:", data)
+        this.groupedData.setState(data, false)
     }
 
     displayAdditionalFields(cardData, additionalFields) {
@@ -156,6 +190,44 @@ class CardInfoCarousel extends PlainComponent {
             `
             this.$('.card-info-additional-fields').innerHTML += additionalField
         })
+    }
+
+    handleAnimation(e, direction) {
+        e.preventDefault()
+
+        this.$('.card-info-wrapper').classList.remove('fade-in')
+        this.$('.card-info-wrapper').classList.remove('fade-left')
+        this.$('.card-info-wrapper').classList.remove('fade-right')
+
+        if (direction === 'prev') {
+            this.$('.card-info-wrapper').classList.add('fade-left')
+            this.$('.controls-wrapper').style.opacity = 0
+            this.$('.card-info-wrapper').style.overflowY = 'hidden'
+            this.$('.card-info-wrapper').onanimationend = () => {
+                this.$('.card-info-wrapper').classList.remove('fade-left')
+                this.$('.card-info-wrapper').classList.add('fade-in')
+                this.$('.card-info-wrapper').onanimationend = () => {
+                    this.$('.card-info-wrapper').classList.remove('fade-in')
+                    this.$('.controls-wrapper').style.opacity = 1
+                    this.$('.card-info-wrapper').style.overflowY = 'auto'
+                }
+            }
+        }
+
+        if (direction === 'next') {
+            this.$('.card-info-wrapper').classList.add('fade-right')
+            this.$('.controls-wrapper').style.opacity = 0
+            this.$('.card-info-wrapper').style.overflowY = 'hidden'
+            this.$('.card-info-wrapper').onanimationend = () => {
+                this.$('.card-info-wrapper').classList.remove('fade-right')
+                this.$('.card-info-wrapper').classList.add('fade-in')
+                this.$('.card-info-wrapper').onanimationend = () => {
+                    this.$('.card-info-wrapper').classList.remove('fade-in')
+                    this.$('.controls-wrapper').style.opacity = 1
+                    this.$('.card-info-wrapper').style.overflowY = 'auto'
+                }
+            }
+        }
     }
 }
 
