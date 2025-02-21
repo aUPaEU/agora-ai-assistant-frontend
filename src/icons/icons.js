2793fe27cc191441c661b435c93f0322128bdde4
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-02-21 13:15:59 +0100

    style: add new icons

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -174,3 +174,19 @@ export const SEARCH = html`
     <path d="M16 16L20 20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
 `
+
+export const CLOSE = html`
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M18 6L6 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+`
+
+export const WEBSITE_LINK = html`
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="3" y="4" width="18" height="16" rx="2" stroke="currentColor" stroke-width="2"/>
+    <path d="M3 8H21" stroke="currentColor" stroke-width="2" stroke-linejoin="round"/>
+    <path d="M7 6H7.01" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+    <path d="M11 6H11.01" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+`
