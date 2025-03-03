155f9170a906e1a481e1db2613d2664c27715212
Josep <jmartinbolet@gmail.com>
2025-03-03 10:21:45 +0100

    refactor: add comments to clarify code

--- src/components/layout/v2/v2.js
+++ src/components/layout/v2/v2.js
@@ -6,6 +6,9 @@ import { PATHS } from "../../../constants/paths.const"
 /* Utils */
 import { html } from "../../../utils/templateTags.util"
 
+/* Icons */
+import { CATALOGUE } from "../../../icons/icons"
+
 class LayoutV2 extends PlainComponent {
     constructor() {
         super('agora-layout-v2', `${PATHS.LAYOUT_COMPONENTS}/v2/v2.css`)
@@ -15,10 +18,10 @@ class LayoutV2 extends PlainComponent {
 
     template() {
         return html`
-            <!-- <img class="fake-header" src="public/assets/images/fake-header.png"/> -->
             <main class="main">
                 <!-- Left Side -->
                 <div class="left">
+                    <!-- Navigator -->
                     <div class="navigator-wrapper">
                         <agora-navigator></agora-navigator>
                     </div>
@@ -26,19 +29,31 @@ class LayoutV2 extends PlainComponent {
 
                 <!-- Right Side -->
                 <div class="right">
+
+                    <!-- Greetings and Showcase -->
                     <div class="showcase">
                         <agora-landing></agora-landing>
                     </div>
+
+                    <!-- Results -->
                     <agora-result-window></agora-result-window>
                 </div>
             </main>
+
+            <!-- Chat or Searchbar -->
             ${
                 this.configContext.getData('enabled_ai')
                 ? html`<agora-chat class="chat"></agora-chat>`
                 : html`<agora-searchbar class="searchbar"></agora-searchbar>`
             }
+
+            <!-- Pin Box -->
             <agora-pin-box></agora-pin-box>
+
+            <!-- Carousel -->
             <agora-card-info-carousel></agora-card-info-carousel>
+
+            <!-- Bottom Fade -->
             <div class="bottom-fade"></div>
         `
     }
