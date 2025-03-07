274adc70e60cf0982d4091c5e3ba1872b2be0105
Josep <jmartinbolet@gmail.com>
2025-03-07 11:05:33 +0100

    update: new bundle 1.0.3

--- agora.config.js
+++ agora.config.js
@@ -1,7 +1,7 @@
 // This could be fetched automatically from the server depending on an environment variable,
 // a cookie or the domain name.
 
-const CURRENT_VERSION = "1.0.2"
+const CURRENT_VERSION = "1.0.3"
 
 export const ENV = {
     LOCAL: {
