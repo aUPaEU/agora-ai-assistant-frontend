b65d22abefb653a2089d95a81ad2a650772eddac
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-04-03 13:15:34 +0200

    chore: new bundle 1.1.1

--- agora.config.js
+++ agora.config.js
@@ -1,7 +1,7 @@
 // This could be fetched automatically from the server depending on an environment variable,
 // a cookie or the domain name.
 
-const CURRENT_VERSION = "1.1.0"
+const CURRENT_VERSION = "1.1.1"
 
 export const ENV = {
     LOCAL: {
