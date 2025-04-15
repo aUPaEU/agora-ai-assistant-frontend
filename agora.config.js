0a7095efd5d3d6f88f245a0dcec37a7104718294
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-04-15 11:22:24 +0200

    feat: update version to 1.1.9 and change CONFIG to LOCAL
    - Updated the version in agora.config.js, package.json, and package-lock.json to 1.1.9.
    - Changed the CONFIG export in agora.config.js to use the LOCAL environment instead of UNITE.
    - Updated bundle.js to reflect the new version and configuration changes.

--- agora.config.js
+++ agora.config.js
@@ -1,7 +1,7 @@
 // This could be fetched automatically from the server depending on an environment variable,
 // a cookie or the domain name.
 
-const CURRENT_VERSION = "1.1.8"
+const CURRENT_VERSION = "1.1.9"
 
 export const ENV = {
     LOCAL: {
@@ -65,4 +65,4 @@ export const ENV = {
     }
 }
 
-export const CONFIG = ENV.UNITE
+export const CONFIG = ENV.LOCAL
