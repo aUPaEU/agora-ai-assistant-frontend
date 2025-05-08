4d9a87ef4119003efbb2269612eed746d058b37d
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-05-08 12:42:23 +0200

    feat: update version to 1.2.0 across configuration files
    - Updated the version number in agora.config.js, package.json, and package-lock.json to reflect the new release version 1.2.0.
    - Adjusted the current version in the bundle.js file to maintain consistency across the application.

--- agora.config.js
+++ agora.config.js
@@ -1,7 +1,7 @@
 // This could be fetched automatically from the server depending on an environment variable,
 // a cookie or the domain name.
 
-const CURRENT_VERSION = "1.1.14"
+const CURRENT_VERSION = "1.2.0"
 
 export const ENV = {
     LOCAL: {
